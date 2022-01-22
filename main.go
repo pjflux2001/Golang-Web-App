1ba51e68dfcbac81376c73be48ae62dfe5c2b285
pjflux2001 <f20180332@goa.bits-pilani.ac.in>
2022-01-22 15:23:30 +0530

    timeout with handlers

--- main.go
+++ main.go
@@ -3,13 +3,36 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func helloWorldPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World, Golang!")
+	fmt.Fprint(w, "<h1>Hello World, Golang!</h1>\n")
+	switch r.URL.Path {
+	case "/":
+		fmt.Fprint(w, "<h1>Hello</h1>"+" /\n"+r.UserAgent()+"\n"+r.Referer()+"\n")
+	case "/ninja":
+		fmt.Fprint(w, "Ninja here")
+	default:
+		fmt.Fprint(w, "Err")
+	}
+}
+
+func timeout(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Timeout Attempt")
+	time.Sleep(2 * time.Second)
+	fmt.Fprint(w, "Did not timeout")
 }
 
 func main() {
 	http.HandleFunc("/", helloWorldPage)
+	http.HandleFunc("/timeout", timeout)
+	// server := http.Server{
+	// 	Addr:         ":8080",
+	// 	Handler:      nil,
+	// 	ReadTimeout:  1000,
+	// 	WriteTimeout: 1000,
+	// }
 	http.ListenAndServe(":8080", nil)
+	// server.ListenAndServe()
 }
